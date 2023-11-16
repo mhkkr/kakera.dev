350f6f0f7f324c43dbb8c987d15088d2c3bc743f
kakera <mokuhen@gmail.com>
2023-11-16 13:42:14 +0900

    fix astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://kakera.dev',
+  base: '/blank',
   integrations: [tailwind()]
 });
