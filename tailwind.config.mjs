bb7d4b333dc17e8d9f45489a3d7ac6b9cd4615a6
kakera <mokuhen@gmail.com>
2024-05-16 18:00:29 +0900

    update トップページ改修

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,8 +6,12 @@ export default {
 	content: ['./src/**/*.{astro,html,js,json,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
     extend: {
+      textColor: {
+        lighten: '#252525',
+        darken: '#ffffff'
+      },
       fontFamily: {
-				roboto: ['Roboto', 'sans-serif']
+				'noto-sans-jp': ['Noto Sans JP', 'sans-serif'],
       },
       zIndex: {
         header: 100
